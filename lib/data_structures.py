56ee96fba514b76f75ff9bfa181c7b5a6b029139
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-06-08 15:33:11 -0400

    completed lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -26,19 +26,18 @@ def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
         print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
 
+def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
         if food["cuisine"] == cuisine:
             return food
 
 def print_spiciest_foods(spicy_foods):
-    for food in spicy_foods:
-        if food["heat_level"] > 5:
-            print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
 
 def get_average_heat_level(spicy_foods):
     return sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods)
-
-def create_spicy_food(spicy_foods, spicy_food):
-    spicy_foods.append(spicy_food)
-    return spicy_foods
